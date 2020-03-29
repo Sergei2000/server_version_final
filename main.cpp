4b35d865bf2a6932dd3b27d4476aba60fcb5d800
Sergei2000 <dsdem@mail.ru>
2020-03-29 11:36:09 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include<boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/thread/recursive_mutex.hpp>
+#include <system_error>
+using namespace boost::asio;
+static io_service service;
+static ip::tcp::acceptor acceptor{service, ip::tcp::endpoint{ip::tcp::v4(), 8001}};
+typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
+boost::recursive_mutex cs,ds;
+static std::vector<std::string> client_list;
+struct client{
+    ip::tcp::socket _sock{service};
+    bool _status=true;
+    bool _initial = true;
+    int _clients_num=0;
+    std::string _reply,_name,_request;
+    void read_request()
+    {
+        boost::asio::streambuf buffer{};
+        read_until(_sock, buffer, "\n");
+        std::string request((std::istreambuf_iterator<char>(&buffer)), std::istreambuf_iterator<char>());
+        _request=request;
+
+    }
+    void write_reply(std::string reply)
+    {
+        reply+="\r\n";
+        _sock.write_some(boost::asio::buffer(reply));
+
+    }
+    void reply_name()
+    {
+        read_request();
+        _name=_request;
+        write_reply(_name);
+        std::cout<<_name;
+        boost::recursive_mutex::scoped_lock lk(ds);
+        client_list.push_back(_name);
+    }
+    void  ping()
+    {
+        write_reply("ping_ok\n");
+    }
+
+    void analyse_request()
+    {
+       read_request();
+       if (_request == "ping\n\r\n")
+       {
+           boost::recursive_mutex::scoped_lock lk(ds);
+           if (_clients_num != client_list.size())
+          { write_reply("changed\n");_clients_num=client_list.size();}
+           else{
+               ping();
+           }
+       } else{
+           if (_request == "list\n\r\n")
+           {
+               std::string list;
+               boost::recursive_mutex::scoped_lock lk(ds);
+               for (int i=0;i<client_list.size();++i)
+               {
+                   list+=client_list[i];
+               }
+               write_reply(list);
+           }
+           else{
+               ping();
+           }
+
+       }
+    }
+    void communicate()
+    {
+        try
+        {
+            if (_initial)
+            {
+                reply_name();
+                _initial=false;
+            }
+           analyse_request();
+        }
+        catch (...)
+        {
+            _status=false;
+            std::cout<<"socket otkinulsya"<<std::endl;
+            _sock.close();
+            boost::recursive_mutex::scoped_lock lk(ds);
+            for (auto i=client_list.begin();i!=client_list.end();)
+            {
+                if (i->data() == _name)
+                {
+                    i=client_list.erase(i);
+                }
+                else{
+                    ++i;
+                }
+            }
+        }
+    }
+
+};
+typedef boost::shared_ptr<client> client_ptr;
+static std::vector<client_ptr> clients;
+void access_func()
+{
+    fflush(stdout);
+    while(true)
+    {
+        client_ptr one(new client);
+        acceptor.accept(one->_sock);
+        boost::recursive_mutex::scoped_lock lk(cs);
+        clients.push_back(one);
+        std::cout<<"zakinul"<<std::endl;
+    }
+}
+
+void communication_with_server()
+{
+    while (true)
+    {
+        boost::this_thread::sleep( boost::posix_time::millisec(1));
+        boost::recursive_mutex::scoped_lock lk(cs);
+
+        for (auto &a: clients)
+        {
+            a->communicate();
+        }
+        for (auto i=clients.begin();i!=clients.end();)
+        {
+            if (!(i->get()->_status))
+            {
+                i=clients.erase(i);
+            }
+            else{
+                ++i;
+            }
+        }
+    }
+}
+int main()
+{
+    boost::thread first(access_func);
+    boost::thread second(communication_with_server);
+    second.join();
+    first.join();
+    return 0;
+}
